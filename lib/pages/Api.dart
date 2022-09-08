3dfbf31e8b13682b0312bae841b81f56e64d7c3c
Joemar Carganillo <carganillo@gmail.com>
2022-09-08 16:38:18 +0800

    joms_

--- lib/pages/Api.dart
+++ lib/pages/Api.dart
@@ -9,6 +9,31 @@ class ApiCon {
   static String storeUrl = '/places?page=0';
   static String recentUrl = '/users/currentUser/orders?pageSize=1&pageNumber=1';
 
-  static String paymenturl = 'https://paypage.sandbox.ngenius-payments.com/';
-  // static String paymenturl = 'https://paypage.ngenius-payments.com/';
+  static String baseurl1() {
+    bool isPro = false;
+
+    String url;
+    String prepro = 'https://drinklink-preprod-be.azurewebsites.net/api';
+    String pro = 'https://drinklink-prod-be.azurewebsites.net/api';
+    if (isPro == true) {
+      url = pro;
+    } else {
+      url = prepro;
+    }
+    return url;
+  }
+
+  static String paymenturl() {
+    bool isPro = false;
+
+    String url;
+    String prepro = 'https://paypage.sandbox.ngenius-payments.com/';
+    String pro = 'https://paypage.ngenius-payments.com/';
+    if (isPro == true) {
+      url = pro;
+    } else {
+      url = prepro;
+    }
+    return url;
+  }
 }
