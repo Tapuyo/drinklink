b3e48401c07d0d86baa0a616ea3f75a9c0c93c49
Joemar Carganillo <carganillo@gmail.com>
2022-09-19 14:33:26 +0800

    Spacing

--- lib/pages/home/savecard.dart
+++ lib/pages/home/savecard.dart
@@ -7,13 +7,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
 import 'package:rflutter_alert/rflutter_alert.dart';
 
-
-
-
 class SaveCardWeb extends StatefulWidget {
   String url;
-
-
 
   SaveCardWeb(this.url);
   @override
@@ -26,38 +21,35 @@ class WebViewExampleState extends State<SaveCardWeb> {
   WebViewExampleState(this.murl);
   final flutterWebViewPlugin = FlutterWebviewPlugin();
 
-
   @override
   void initState() {
     super.initState();
     final flutterWebviewPlugin = new FlutterWebviewPlugin();
     flutterWebViewPlugin.onUrlChanged.listen((String url) {
-      print("This is url: "+ url);
-      if(url != murl){
+      print("This is url: " + url);
+      if (url != murl) {
         //Navigator.pop(context, url);
         checkUrlRes(url);
       }
     });
   }
 
-  checkUrlRes(String url)async {
+  checkUrlRes(String url) async {
     bool checkurl = url.contains('paidCard');
     bool checkurl1 = url.contains('UnpaidCard');
 
-    if(checkurl == true && checkurl1 == false){
-        // var divurl = url.split('=');
-        // print(divurl[2].trim());
-        // String codena = divurl[2].trim();
-        // String mycode = codena.replaceAll('&paymentRef', '');
-        // print(mycode);
-        Navigator.pop(context, 'Added');
-      }else if(checkurl == true && checkurl1 == true){
-         Navigator.pop(context, 'Failed');
-      }
-        else{
-        Navigator.pop(context, 'Failed');
-      }
-    
+    if (checkurl == true && checkurl1 == false) {
+      // var divurl = url.split('=');
+      // print(divurl[2].trim());
+      // String codena = divurl[2].trim();
+      // String mycode = codena.replaceAll('&paymentRef', '');
+      // print(mycode);
+      Navigator.pop(context, 'Added');
+    } else if (checkurl == true && checkurl1 == true) {
+      //  Navigator.pop(context, 'Failed');
+    } else {
+      // Navigator.pop(context, 'Failed');
+    }
 
     // Map<String, String> headers = {"Content-Type": "application/json; charset=utf-8"};
     // final response = await http.get(url,headers: headers);
@@ -84,25 +76,27 @@ class WebViewExampleState extends State<SaveCardWeb> {
     //
     //   }
     // }
-
   }
-
 
   @override
   Widget build(BuildContext context) {
     return WebviewScaffold(
-
       url: murl,
       appBar: new AppBar(
         backgroundColor: Color(0xFF2b2b61),
-        title: new Text("Save Card",style: TextStyle(fontSize: 20, color: Colors.white),),
-        leading:  IconButton(
-          icon: Icon(Icons.arrow_back, color: Colors.white,),
+        title: new Text(
+          "Save Card",
+          style: TextStyle(fontSize: 20, color: Colors.white),
+        ),
+        leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back,
+            color: Colors.white,
+          ),
           onPressed: () {
             Navigator.pop(context, 'failed');
           },
         ),
-
       ),
       initialChild: Container(
         color: Colors.white,
@@ -113,4 +107,3 @@ class WebViewExampleState extends State<SaveCardWeb> {
     );
   }
 }
-
