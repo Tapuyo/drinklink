f0bd587d68cf785889f2d736ca151a4672b58723
Joms1122 <98512522+Joms1122@users.noreply.github.com>
2022-10-14 20:58:00 +0800

    joms_v55_1202979916543382_message_dialog_in_help_center_v1

--- lib/pages/login/help.dart
+++ lib/pages/login/help.dart
@@ -56,7 +56,7 @@ class _helpignPageState extends State<help> {
     return true;
   }
 
-  _clear() async {
+  _clear() {
     // nameController.text = '';
     // emailController.text = '';
     messageController.text = '';
@@ -64,30 +64,12 @@ class _helpignPageState extends State<help> {
 
   main() async {
     _loadPreview();
-    // Note that using a username and password for gmail only works if
-    // you have two-factor authentication enabled and created an App password.
-    // Search for "gmail app password 2fa"
-    // The alternative is to use oauth.
+
     String username = 'leepe@drinklinkph.com';
     String password = 'P@ssw0rd';
 
     final smtpServer =
         SmtpServer('plesk5600.is.cc', username: username, password: password);
-    // final smtpServer = gmail(username, password);
-    // Use the SmtpServer class to configure an SMTP server:
-    // final smtpServer = SmtpServer('smtp.domain.com');
-    // See the named arguments of SmtpServer for further configuration
-    // options.
-
-    // Create our message.
-    // final message = Message()
-    //   ..from = Address(username, 'jo.leepeoutsourcing@gmail.com')
-    //   ..recipients.add('jo.leepeoutsourcing@gmail.com')
-    //   // ..ccRecipients.addAll(['destCc1@example.com', 'destCc2@example.com'])
-    //   // ..bccRecipients.add(Address('bccAddress@example.com'))
-    //   ..subject = 'DrinkLink Help Center:: ${DateTime.now()}'
-    //   ..text = 'This is the plain text.\nThis is line 2 of the text part.'
-    //   ..html = "<h1>Test</h1>\n<p>Hey! Here's some HTML content</p>";
 
     final message = Message()
       ..from = Address(username, emailController.text)
@@ -104,59 +86,13 @@ class _helpignPageState extends State<help> {
 
     try {
       final sendReport = await send(message, smtpServer);
-      if (sendReport.toString().contains('successfully sent')) {
-        Navigator.pop(context);
-        _messageDialog('Help Center', 'Your message was sent successfully!', '',
-            'Send Again');
-        _clear();
-      }
       print('Message sent: ' + sendReport.toString());
     } on MailerException catch (e) {
-      Navigator.pop(context);
       print('Message not sent.');
       for (var p in e.problems) {
         print('Problem: ${p.code}: ${p.msg}');
       }
-      _messageDialog('Help Center', 'Failed to send message!', '', 'Ok');
     }
-
-    // DONE
-
-    // Let's send another message using a slightly different syntax:
-    //
-    // Addresses without a name part can be set directly.
-    // For instance `..recipients.add('destination@example.com')`
-    // If you want to display a name part you have to create an
-    // Address object: `new Address('destination@example.com', 'Display name part')`
-    // Creating and adding an Address object without a name part
-    // `new Address('destination@example.com')` is equivalent to
-    // adding the mail address as `String`.
-    // final equivalentMessage = Message()
-    //   ..from = Address(username, 'Your name 😀')
-    //   ..recipients.add(Address('destination@example.com'))
-    //   ..ccRecipients
-    //       .addAll([Address('destCc1@example.com'), 'destCc2@example.com'])
-    //   ..bccRecipients.add('bccAddress@example.com')
-    //   ..subject = 'Test Dart Mailer library :: 😀 :: ${DateTime.now()}'
-    //   ..text = 'This is the plain text.\nThis is line 2 of the text part.'
-    //   ..html =
-    //       '<h1>Test</h1>\n<p>Hey! Here is some HTML content</p><img src="cid:myimg@3.141"/>'
-    //   ..attachments = [
-    //     FileAttachment(File('exploits_of_a_mom.png'))
-    //       ..location = Location.inline
-    //       ..cid = '<myimg@3.141>'
-    //   ];
-
-    // final equivalentMessage = Message()
-    //   ..from = Address(username, nameController.text + ' : support ')
-    //   ..recipients.add(Address('jo.leepeoutsourcing@gmail.com'))
-    //   // ..ccRecipients
-    //   //     .addAll([Address('destCc1@example.com'), 'destCc2@example.com'])
-    //   // ..bccRecipients.add('bccAddress@example.com')
-    //   ..subject = 'Test Dart Mailer library :: 😀 :: ${DateTime.now()}'
-    //   ..text = 'This is the plain text.\nThis is line 2 of the text part.'
-    //   ..html =
-    //       '<h1>Test</h1>\n<p>Hey! Here is some HTML content</p><img src="cid:myimg@3.141"/>';
 
     final equivalentMessage = Message()
       ..from = Address(username, nameController.text + ' : support ')
@@ -171,18 +107,24 @@ class _helpignPageState extends State<help> {
           "</h1>\n<p>" +
           messageController.text +
           "</p>";
-    final sendReport2 = await send(equivalentMessage, smtpServer);
+    // final sendReport2 = await send(equivalentMessage, smtpServer);
 
-    // Sending multiple messages with the same connection
-    //
-    // Create a smtp client that will persist the connection
     var connection = PersistentConnection(smtpServer);
 
     // Send the first message
-    await connection.send(message);
+    final rsult = await connection.send(message);
+    if (rsult.toString().contains('Message successfully sent')) {
+      Navigator.of(context).pop();
+      _messageDialog('Help Center', 'Your message was sent successfully!', '',
+          'Send Again');
+      _clear();
+    } else {
+      Navigator.of(context).pop();
+      _messageDialog('Help Center', 'Failed to send message!', '', 'Ok');
+    }
 
     // send the equivalent message
-    await connection.send(equivalentMessage);
+    // await connection.send(equivalentMessage);
 
     // close the connection
     await connection.close();
