449d741af467857797bc4a4412d4e4f4d68d290f
Joms1122 <98512522+Joms1122@users.noreply.github.com>
2023-01-13 16:30:49 +0800

    change logo text

--- lib/pages/splash/splash_page.dart
+++ lib/pages/splash/splash_page.dart
@@ -7,49 +7,60 @@ import 'package:driklink/routes/routes.dart';
 import 'package:driklink/utils/constants.dart';
 import 'package:flutter/material.dart';
 
-
 class SplashPage extends StatefulWidget {
   @override
   _SplashPageState createState() => _SplashPageState();
 }
 
 class _SplashPageState extends State<SplashPage> {
-
   @override
   void initState() {
     super.initState();
 
-
-
-    Timer(Duration(seconds: 1), () => {
-      Navigator.pushReplacement(
-      context,
-      MaterialPageRoute(builder: (context) => HomePage()),
-      )
-     });
-
+    Timer(
+        Duration(seconds: 1),
+        () => {
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(builder: (context) => HomePage()),
+              )
+            });
   }
-
-
-
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Container(
-        decoration: BoxDecoration(
-            image: DecorationImage(
-                image: AssetImage("assets/images/bkgdefault.png"), fit: BoxFit.cover)),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text('DrinkLink', style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 50,decoration: TextDecoration.none,),),
-            Text('More good times.', style: TextStyle(color: Colors.deepOrange, fontSize: 16, fontFamily: 'NunitoSans' ,decoration: TextDecoration.none,),)
-          ],
-        )
-      ),
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage("assets/images/bkgdefault.png"),
+                  fit: BoxFit.cover)),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                'DrinkLink',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 50,
+                  fontFamily: 'Roboto',
+                  decoration: TextDecoration.none,
+                ),
+              ),
+              Text(
+                'More good times.',
+                style: TextStyle(
+                  color: Colors.deepOrange,
+                  fontSize: 16,
+                  fontFamily: 'NunitoSans',
+                  decoration: TextDecoration.none,
+                ),
+              )
+            ],
+          )),
     );
   }
 }
